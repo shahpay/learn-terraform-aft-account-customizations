10b436a9364dbd820455a095a790b219d8fc8574
shahpay <shahpay@amazon.co.uk>
2023-06-06 17:16:02 +0100

    modified main.tf

--- dtpl-dev/terraform/main.tf
+++ dtpl-dev/terraform/main.tf
@@ -1,3 +1,13 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "eu-west-2" {
+    provider = aws.euwest2
+}
+
+data "aws_region" "eu-west-1" {
+    provider = aws.euwest1
+}
+
 resource "aws_securityhub_standards_control" "preventthis" {
   provider = aws.euwest1
   standards_control_arn = "arn:aws:securityhub:${data.aws_region.eu-west-1.name}:${data.aws_caller_identity.current.account_id}:control/cis-aws-foundations-benchmark/v/1.2.0/2.3"
