4e68be843fd8c93906c9c83706f98e740a3a9d5a
shahpay <shahpay@amazon.co.uk>
2023-06-08 14:32:41 +0100

    added security hub modules

--- dtpl-dev/terraform/modules/securityhub/main.tf
+++ dtpl-dev/terraform/modules/securityhub/main.tf
@@ -31,5 +31,5 @@ resource "aws_securityhub_standards_control" "disabled_control_all" {
    for_each = {for index in var.disabled_control_all:  index.control_id => index}
    control_status = "${contains(var.enabled_control_all, "${each.value.control_id}")}" ? "ENABLED" : "DISABLED"
    disabled_reason  = "${contains(var.enabled_control_all, "${each.value.control_id}")}" ? "disabled reason" : null
-   standards_control_arn = "arn:aws:securityhub:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.name}:control/standards/nist-800-53/v/${locals.nis_version}/${each.value.control_id}"
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.account_id}:control/standards/nist-800-53/v/${locals.nis_version}/${each.value.control_id}"
 }
