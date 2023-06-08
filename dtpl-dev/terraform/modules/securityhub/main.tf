ece2d83101d875be8243405fbd7f23cc446ecd01
shahpay <shahpay@amazon.co.uk>
2023-06-08 18:37:26 +0100

    modified the main.tf

--- dtpl-dev/terraform/modules/securityhub/main.tf
+++ dtpl-dev/terraform/modules/securityhub/main.tf
@@ -24,7 +24,14 @@ locals  {
  disabled_reason = "Not needed for our usecase"
 }
 
-data "aws_region" "current_region" {  }
+data "aws_region" "euwest1" { 
+    provider = aws.euwest1
+ }
+
+data "aws_region" "euwest2" { 
+    provider = aws.euwest2
+ }
+
 
 data "aws_caller_identity" "current_account" {}
 
@@ -33,7 +40,7 @@ resource "aws_securityhub_standards_control" "evaluate_controls" {
    for_each = var.disabled_nis_control_all_region
    control_status = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? "ENABLED" : "DISABLED"
    disabled_reason  = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? null : local.disabled_reason
-   standards_control_arn = "arn:aws:securityhub:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.euwest1.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
 }
 
 resource "aws_securityhub_standards_control" "evaluate_controls" {
@@ -41,5 +48,5 @@ resource "aws_securityhub_standards_control" "evaluate_controls" {
    for_each = var.disabled_nis_control_all_region
    control_status = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? "ENABLED" : "DISABLED"
    disabled_reason  = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? null : local.disabled_reason
-   standards_control_arn = "arn:aws:securityhub:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.euwest2.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
 }
