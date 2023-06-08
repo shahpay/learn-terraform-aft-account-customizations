94e7221e34ee46e188b3f11ab2ec3680435b28e9
shahpay <shahpay@amazon.co.uk>
2023-06-08 17:46:33 +0100

    modified security hub

--- dtpl-dev/terraform/modules/securityhub/main.tf
+++ dtpl-dev/terraform/modules/securityhub/main.tf
@@ -28,9 +28,9 @@ data "aws_region" "current_region" {  }
 
 data "aws_caller_identity" "current_account" {}
 
-resource "aws_securityhub_standards_control" "disabled_control_all" {
+resource "aws_securityhub_standards_control" "evaluate_controls" {
    for_each = var.disabled_nis_control_all_region
    control_status = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? "ENABLED" : "DISABLED"
    disabled_reason  = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? null : local.disabled_reason
-   standards_control_arn = "arn:aws:securityhub:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.account_id}:control/standards/nist-800-53/v/${local.nis_version}/${each.value}"
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
 }
