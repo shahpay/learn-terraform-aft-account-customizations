0b8e1403f4c513448b7dd6323d8dc344b2f2a1d8
shahpay <shahpay@amazon.co.uk>
2023-06-08 11:09:59 +0100

    dtpl-dev

--- dtpl-dev/terraform/main.tf
+++ dtpl-dev/terraform/main.tf
@@ -14,3 +14,17 @@ resource "aws_securityhub_standards_control" "preventthis" {
   control_status        = "DISABLED"
   disabled_reason       = "We handle password policies within Okta"
 }
+
+resource "aws_securityhub_standards_control" "preventthis1" {
+  provider = aws.euwest2
+  standards_control_arn = "arn:aws:securityhub:${data.aws_region.eu-west-2.name}:${data.aws_caller_identity.current.account_id}:control/cis-aws-foundations-benchmark/v/1.2.0/2.3"
+  control_status        = "DISABLED"
+  disabled_reason       = "We handle password policies within Okta"
+}
+
+resource "aws_securityhub_standards_control" "preventthis2" {
+  provider = aws.euwest1
+  standards_control_arn = "arn:aws:securityhub:${data.aws_region.eu-west-1.name}:${data.aws_caller_identity.current.account_id}:control/nist-800-53/v/5.0.0/APIGateway.4"
+  control_status        = "DISABLED"
+  disabled_reason       = "We handle password policies within Okta"
+}
