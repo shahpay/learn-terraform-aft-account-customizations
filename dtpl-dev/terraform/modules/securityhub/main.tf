e16effc68284a3593b19e2910e6e3bec97450b91
shahpay <shahpay@amazon.co.uk>
2023-06-08 18:14:34 +0100

    modified main.tf

--- dtpl-dev/terraform/modules/securityhub/main.tf
+++ dtpl-dev/terraform/modules/securityhub/main.tf
@@ -14,7 +14,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.20.0"
-      configuration_aliases = [ aws.euwest1, aws.euwest2 ]
+      configuration_aliases = [ aws.region ]
     }
   }
 }
@@ -29,6 +29,7 @@ data "aws_region" "current_region" {  }
 data "aws_caller_identity" "current_account" {}
 
 resource "aws_securityhub_standards_control" "evaluate_controls" {
+   provider = aws.region
    for_each = var.disabled_nis_control_all_region
    control_status = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? "ENABLED" : "DISABLED"
    disabled_reason  = "${can(var.enabled_nis_control_all_region["${each.key}"])}" ? null : local.disabled_reason
