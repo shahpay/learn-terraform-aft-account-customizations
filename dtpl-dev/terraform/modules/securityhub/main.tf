28a7e82014ea682cbdd67bf7ec4d8c2846039ba9
shahpay <shahpay@amazon.co.uk>
2023-06-08 20:00:41 +0100

    modified main.tf

--- dtpl-dev/terraform/modules/securityhub/main.tf
+++ dtpl-dev/terraform/modules/securityhub/main.tf
@@ -1,13 +1,3 @@
-# provider "aws" {
-#   region = var.aws_region
-#   default_tags {
-#     tags = {
-#       Environment = "Test"
-#       Name        = "Provider Tag"
-#     }
-#   }
-# }
-
 terraform {
   required_version = ">= 0.13.1"
   required_providers {
@@ -52,6 +42,46 @@ data "aws_region" "eu-west-2" {
 
 
 data "aws_caller_identity" "current_account" {}
+
+resource "aws_securityhub_standards_control" "evaluate_controls_useast1" {
+   for_each = var.disabled_nis_control_all_region
+   control_status = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_useast1["${each.key}"])}" ) ? "ENABLED" : "DISABLED"
+   disabled_reason  = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_useast1["${each.key}"])}" ) ? null : local.disabled_reason
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.us-east-1.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
+}
+
+
+resource "aws_securityhub_standards_control" "evaluate_controls_useast2" {
+   provider = aws.useast2
+   for_each = var.disabled_nis_control_all_region
+   control_status = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_useast2["${each.key}"])}" ) ? "ENABLED" : "DISABLED"
+   disabled_reason  = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_useast2["${each.key}"])}" ) ? null : local.disabled_reason
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.us-east-2.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
+}
+
+resource "aws_securityhub_standards_control" "evaluate_controls_uswest2" {
+   provider = aws.uswest2
+   for_each = var.disabled_nis_control_all_region
+   control_status = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_uswest2["${each.key}"])}" ) ? "ENABLED" : "DISABLED"
+   disabled_reason  = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_uswest2["${each.key}"])}" ) ? null : local.disabled_reason
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.us-west-2.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
+}
+
+resource "aws_securityhub_standards_control" "evaluate_controls_cacentral1" {
+   provider = aws.cacentral1
+   for_each = var.disabled_nis_control_all_region
+   control_status = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_cacentral1["${each.key}"])}" ) ? "ENABLED" : "DISABLED"
+   disabled_reason  = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_cacentral1["${each.key}"])}" ) ? null : local.disabled_reason
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.ca-central-1.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
+}
+
+resource "aws_securityhub_standards_control" "evaluate_controls_eucentral1" {
+   provider = aws.eucentral1
+   for_each = var.disabled_nis_control_all_region
+   control_status = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_eucentral1["${each.key}"])}" ) ? "ENABLED" : "DISABLED"
+   disabled_reason  = ( "${can(var.enabled_nis_control_all_region["${each.key}"])}" || "${can(var.enabled_nis_control_eucentral1["${each.key}"])}" ) ? null : local.disabled_reason
+   standards_control_arn = "arn:aws:securityhub:${data.aws_region.eu-central-1.name}:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/${each.value}"
+}
 
 resource "aws_securityhub_standards_control" "evaluate_controls_euwest1" {
    provider = aws.euwest1
