a75c6875c62fe4ad082c4decf2ad1e07c4134398
shahpay <shahpay@amazon.co.uk>
2023-06-10 19:15:51 +0100

    modified disable nist controls

--- dtpl-dev/terraform/disable_specific_nist_controls.tf
+++ dtpl-dev/terraform/disable_specific_nist_controls.tf
@@ -1,14 +1,14 @@
 variable disabled_nis_control_all_region {
 type = map(string)
 default = {
-  nis_elb_5    =     ELB.5
-  nis_rds_19   =     RDS.19
-  nis_rds_20   =     RDS.20
-  nis_rds_21   =     RDS.21
-  nis_rds_22   =     RDS.22
-  nis_rds_7    =     RDS.7
-  nis_rds_8    =     RDS.8
-  nis_rds_9    =     RDS.9
+  nis_elb_5    =     "ELB.5"
+  nis_rds_19   =     "RDS.19"
+  nis_rds_20   =     "RDS.20"
+  nis_rds_21   =     "RDS.21"
+  nis_rds_22   =     "RDS.22"
+  nis_rds_7    =     "RDS.7"
+  nis_rds_8    =     "RDS.8"
+  nis_rds_9    =     "RDS.9"
 }
 }
 
